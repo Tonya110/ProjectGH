7c455b22b1ad13c581d6ed4e720baa6cd6c4504b
tonya110_github <tonya110@yandex.ru>
2017-06-07 03:46:14 +0500

    Разработка функций

--- GitHubProgect/GitHubProgect/Processing.cs
+++ GitHubProgect/GitHubProgect/Processing.cs
@@ -8,5 +8,30 @@ namespace GitHubProgect
 {
     class Processing
     {
+        double rezult = 0;
+
+        public int Summa(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Minus(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public int Umnogit(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public double Delit(int number1, int number2)
+        {
+            if (number2 != 0)
+           {
+                rezult = number1 / number2;
+           }
+            return rezult;
+        }
     }
 }
